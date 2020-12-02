107b47a096277f30d1a8bd32d39ebffb00f3e6e0
Jackwang <78700393@qq.com>
2020-12-02 17:13:38 +0800

    fileType init

---
+++ src/main/java/com/github/jackbytes/common/fileType/FileTypeUtil.java
@@ -0,0 +1,13 @@
+package com.github.jackbytes.common.fileType;
+
+/**
+ * 识别文件类型,后缀,MIMETYPE,通过后缀获取MIMETYPE(没法通过MIMETYPE获取后缀,因为一个MIMETYPE可能对应多个后缀名);
+ * 参考 https://blog.csdn.net/pangliang_csdn/article/details/90641424
+ *
+ * @author Jack Wang
+ */
+public class FileTypeUtil {
+
+
+
+}
