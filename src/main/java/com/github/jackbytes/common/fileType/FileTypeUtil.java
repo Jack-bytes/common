a204d2ca39c2536a3f46943a5c794dca33f8de47
Jackwang <78700393@qq.com>
2020-12-09 16:28:52 +0800

    general

--- src/main/java/com/github/jackbytes/common/fileType/FileTypeUtil.java
+++ src/main/java/com/github/jackbytes/common/fileType/FileTypeUtil.java
@@ -3,6 +3,7 @@ package com.github.jackbytes.common.fileType;
 /**
  * 识别文件类型,后缀,MIMETYPE,通过后缀获取MIMETYPE(没法通过MIMETYPE获取后缀,因为一个MIMETYPE可能对应多个后缀名);
  * 参考 https://blog.csdn.net/pangliang_csdn/article/details/90641424
+ * https://blog.csdn.net/weixin_41984330/article/details/107008554
  *
  * @author Jack Wang
  */
