ea2bcc13a4c7d183f0d093f4778042e6556f7f93
Jackwang <78700393@qq.com>
2021-03-17 15:14:54 +0800

    save

---
+++ src/main/java/cn/com/lezz/common/fileType/FileCommon.java
@@ -0,0 +1,17 @@
+package cn.com.lezz.common.fileType;
+
+import java.io.File;
+
+public class FileCommon {
+
+    public static void deleteFile(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                deleteFile(f);
+            }
+        }
+        file.delete();
+    }
+
+}
--- src/main/java/cn/com/lezz/common/fileType/FileTypeUtil.java
+++ src/main/java/cn/com/lezz/common/fileType/FileTypeUtil.java
@@ -1,4 +1,4 @@
-package com.github.jackbytes.common.fileType;
+package cn.com.lezz.common.fileType;
 
 /**
  * 识别文件类型,后缀,MIMETYPE,通过后缀获取MIMETYPE(没法通过MIMETYPE获取后缀,因为一个MIMETYPE可能对应多个后缀名);
@@ -8,7 +8,6 @@ package com.github.jackbytes.common.fileType;
  * @author Jack Wang
  */
 public class FileTypeUtil {
-
 
 
 }
